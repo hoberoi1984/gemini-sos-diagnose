c2911f33504e39ade8248ca6cc2e9c76c8f90b7b
hoberoi1984 <h.oberoi@osttra.com>
2026-05-17 13:17:47 +0530

    Delete gemini-sos-analyzer directory

--- gemini-sos-analyzer/dashboard/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SOSViewer from './SOSViewer'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <SOSViewer />
-  </React.StrictMode>,
-)
